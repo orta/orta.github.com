3fed7658d221742cb231e988ebbabe1d4485bf8e
Orta Therox <orta.therox@gmail.com>
2020-01-18 13:00:47 -0500

    Use yarn and typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home = () => (
     <div className="hero">
       <h1 className="title">Welcome to Next.js!</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
+        To get , edit <code>pages/index.js</code> and save to reload.
       </p>
 
       <div className="row">
